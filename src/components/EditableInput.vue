e5c98547a867859a3d65171906114f9f5b58a753
xiaokai <kexiaokai@gmail.com>
2016-04-21 20:26:47 +0800

    [UPD] update editable input

--- src/components/EditableInput.vue
+++ src/components/EditableInput.vue
@@ -18,82 +18,40 @@ export default {
   },
   data () {
     return {
+      arrowOffset: 1
     }
   },
   methods: {
     handleBlur () {
-      if (this.state.blurValue) {
-        this.setState({ value: this.state.blurValue, blurValue: null })
-      }
+      
     },
     handleChange (e) {
-      if (this.props.label !== null) {
-        var obj = {}
-        obj[this.props.label] = e.target.value
-        this.props.onChange(obj)
-      } else {
-        this.props.onChange(e.target.value)
-      }
-
-      this.setState({ value: e.target.value })
+      
     },
     handleKeyDown (e) {
-      var number = Number(e.target.value)
+      var val = this.val;
+
+      var number = Number(val)
       if (number) {
-        var amount = this.props.arrowOffset || 1
+        var amount = this.arrowOffset || 1
 
         // Up
         if (e.keyCode === 38) {
-          if (this.props.label !== null) {
-            var obj = {}
-            obj[this.props.label] = number + amount
-            this.props.onChange(obj)
-          } else {
-            this.props.onChange(number + amount)
-          }
-
-          this.setState({ value: number + amount })
+          this.val = number + amount
         }
 
         // Down
         if (e.keyCode === 40) {
-          if (this.props.label !== null) {
-            var obj = {}
-            obj[this.props.label] = number - amount
-            this.props.onChange(obj)
-          } else {
-            this.props.onChange(number - amount)
-          }
-
-          this.setState({ value: number - amount })
+          this.val = number - amount
         }
 
       }
     },
     handleDrag (e) {
-      if (this.props.dragLabel) {
-        var newValue = Math.round(this.props.value + e.movementX)
-        if (newValue >= 0 && newValue <= this.props.dragMax) {
-          var obj = {}
-          obj[this.props.label] = newValue
-          this.props.onChange(obj)
-        }
-      }
+      
     },
     handleMouseDown (e) {
-      if (this.props.dragLabel) {
-        e.preventDefault()
-        this.handleDrag(e)
-        window.addEventListener('mousemove', this.handleDrag)
-        window.addEventListener('mouseup', this.handleMouseUp)
-      }
-    },
-    handleMouseUp () {
-      this.unbindEventListeners()
-    },
-    unbindEventListeners () {
-      window.removeEventListener('mousemove', this.handleChange)
-      window.removeEventListener('mouseup', this.handleMouseUp)
+      
     }
   }
 
