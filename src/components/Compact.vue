dfaef0b2218d4590d31f592aaacfa51139153b0e
xiaokai <kexiaokai@gmail.com>
2016-04-22 13:27:21 +0800

    [UPD] add computed pick

--- src/components/Compact.vue
+++ src/components/Compact.vue
@@ -5,9 +5,10 @@
         @click="handlerClick(c)"
         :class="{white: c === '#FFFFFF' }"
         :style="{background: c}">
-        <div class="dot" v-show="c === dot"></div>
+        <div class="dot" v-show="c === pick"></div>
       </li>
     </ul>
+
   </div>
 </template>
 
@@ -17,7 +18,7 @@ import colorMixin from '../mixin/color'
 var colors = [
   '#4D4D4D', '#999999', '#FFFFFF', '#F44E3B', '#FE9200', '#FCDC00',
   '#DBDF00', '#A4DD00', '#68CCCA', '#73D8FF', '#AEA1FF', '#FDA1FF',
-  '#333333', '#808080', '#cccccc', '#D33115', '#E27300', '#FCC400',
+  '#333333', '#808080', '#CCCCCC', '#D33115', '#E27300', '#FCC400',
   '#B0BC00', '#68BC00', '#16A5A5', '#009CE0', '#7B64FF', '#FA28FF',
   '#000000', '#666666', '#B3B3B3', '#9F0500', '#C45100', '#FB9E00',
   '#808900', '#194D33', '#0C797D', '#0062B1', '#653294', '#AB149E',
@@ -31,15 +32,22 @@ export default {
     rgba: Object,
     hsl: Object,
   },
+  computed: {
+    pick () {
+      return this.hex.toUpperCase()
+    }
+  },
   data () {
     return {
       colors: colors,
-      dot: ''
+      pick: ''
     }
   },
   methods: {
     handlerClick (c) {
-      this.dot = c
+      this.colorChange(c, 'hex')
+    },
+    onChange () {
     }
   }
 
