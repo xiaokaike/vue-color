dbb83c95b66dcf24a2ce5cad8120561a3e7d7cc6
xiaokai <kexiaokai@gmail.com>
2016-04-22 13:46:16 +0800

    [UPD] update debug

--- src/Color.vue
+++ src/Color.vue
@@ -1,16 +1,57 @@
 <template>
   <div>
-    color
+    <div class="test-wrap">
+      <p>{{hex}}</p>
+      <p>{{rgba.r}} {{rgba.g}} {{rgba.b}} {{rgba.a}}</p>
+      <p>{{hsl.h}} {{hsl.s}} {{hsl.l}}</p>
+    </div>
+
+    <div class="components-wrap">
+      <material :hex.sync="hex" :rgba.sync="rgba" :hsl.sync="hsl"></material>
+      <h6>Material</h6>
+    </div>
+    
+    <div class="components-wrap">
+      <compact :hex.sync="hex" :rgba.sync="rgba" :hsl.sync="hsl"></compact>
+      <h6>Compact</h6>
+    </div>
+
+    <div class="components-wrap">
+      <swatches :hex.sync="hex" :rgba.sync="rgba" :hsl.sync="hsl"></swatches>
+      <h6>Swatches</h6>
+    </div>
   </div>
 </template>
 
 <script>
+import material from './components/Material.vue'
+import compact from './components/Compact.vue'
+import swatches from './components/Swatches.vue'
+
 export default {
   name: 'Color',
+  components:{
+    material,
+    compact,
+    swatches
+  },
   props: {
   },
   data () {
     return {
+      hex: '#333',
+      rgba: {
+        r: 51,
+        g: 51,
+        b: 51,
+        a: 1,
+      },
+      hsl: {
+        h: 0,
+        s: 0,
+        l: .20,
+        a: 1,
+      }
     }
   }
 
@@ -18,5 +59,18 @@ export default {
 </script>
 
 <style>
-
+.components-wrap{
+  margin-bottom: 10px;
+}
+.components-wrap h6{
+  margin: 0;
+  padding: 5px 0;
+  color: #666;
+}
+.test-wrap {
+  margin-bottom: 10px;
+}
+.test-wrap p{
+  margin: 0;
+}
 </style>
--- src/app.js
+++ src/app.js
@@ -1,7 +1,4 @@
 import color from './Color.vue'
-import material from './components/Material.vue'
-import compact from './components/Compact.vue'
-import swatches from './components/Swatches.vue'
 
 Vue.config.debug = true
 
@@ -9,24 +6,6 @@ new Vue({
 	el: '#app',
 	components: {
     color,
-    material,
-    compact,
-    swatches
-  },
-  data: {
-    hex: '#333',
-    rgba: {
-      r: 51,
-      g: 51,
-      b: 51,
-      a: 1,
-    },
-    hsl: {
-      h: 0,
-      s: 0,
-      l: .20,
-      a: 1,
-    }
   }
 })
 
