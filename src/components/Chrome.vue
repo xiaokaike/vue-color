f29d6fc29d3461fa574150402481b6e18eddcd20
xiaokai <kexiaokai@gmail.com>
2016-04-23 10:59:03 +0800

    [ADD] add some new components

---
+++ src/components/Chrome.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="c-chrome">
+    
+  </div>
+</template>
+
+<script>
+import colorMixin from '../mixin/color'
+
+export default {
+  name: 'Chrome',
+  mixins: [colorMixin],
+  props: {
+  },
+  data () {
+    return {
+      
+    }
+  }
+
+}
+</script>
+
+<style lang="stylus">
+.c-chrome
+  border 1px solid #000
+  background #fff
+  border-radius 2px
+  box-shadow 0 0 2px rgba(0,0,0,.3), 0 4px 8px rgba(0,0,0,.3)
+  box-sizing "initial"
+  width 225px
+  font-family "Menlo"
+  .saturation
+    width 100%
+    padding-bottom 55%
+    position relative
+    border-radius 2px 2px 0 0
+    overflow hidden
+</style>
---
+++ src/components/Photoshop.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+import colorMixin from '../mixin/color'
+
+export default {
+  name: 'Photoshop',
+  mixins: [colorMixin],
+  props: {
+  },
+  data () {
+    return {
+      
+    }
+  }
+
+}
+</script>
+
+<style lang="stylus">
+
+</style>
