c5d12ad71cca5dedebad129712f5d624d990400b
xiaokai <kexiaokai@gmail.com>
2016-04-24 19:55:35 +0800

    [UPD] update build require

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import Color from './Color.vue'
+import Compact from './components/Compact.vue'
+import Material from './components/Material.vue'
+import Slider from './components/Slider.vue'
+import Swatches from './components/Swatches.vue'
+
+import Photoshop from './components/Photoshop.vue'
+import Sketch from './components/Sketch.vue'
+import Chrome from './components/Chrome.vue'
 
 const VueColor = {
-  Color,
+  Compact,
+  Material,
+  Slider,
+  Swatches
 }
 
 module.exports = VueColor
