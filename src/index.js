b05cd29634a4db845ad097be777c60e7350ffd72
xiaokai <kexiaokai@gmail.com>
2016-04-30 21:27:01 +0800

    [UPD] state-2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import Compact from './components/Compact.vue'
 import Material from './components/Material.vue'
 import Slider from './components/Slider.vue'
 import Swatches from './components/Swatches.vue'
-
 import Photoshop from './components/Photoshop.vue'
 import Sketch from './components/Sketch.vue'
 import Chrome from './components/Chrome.vue'
