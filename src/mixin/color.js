83a6c57c12125d748173ab8c7d28e11000fefc24
Cassio Cabral <cassioscabral@gmail.com>
2016-05-11 23:37:09 -0300

    fill the attributes when only hex value is given

--- src/mixin/color.js
+++ src/mixin/color.js
@@ -1,35 +1,42 @@
 import tinycolor from 'tinycolor2'
 
+function _colorChange (data, oldHue) {
+  if (data.a && data.a > 1) {
+    data.a = 1
+  }
+
+  var color = data.hex ? tinycolor(data.hex) : tinycolor(data)
+  var hsl = color.toHsl()
+  var hsv = color.toHsv()
+  if (hsl.s === 0) {
+    hsl.h = oldHue || 0
+    hsv.h = oldHue || 0
+  }
+  return {
+    hsl: hsl,
+    hex: color.toHexString().toUpperCase(),
+    rgba: color.toRgb(),
+    hsv: hsv,
+    oldHue: data.h || oldHue || hsl.h,
+    source: data.source,
+    a: data.a
+  }
+}
+
 export default {
   props: {
     colors: Object
   },
   created () {
     // console.log(this.colors)
-    this.colors.hex = this.colors.hex.toUpperCase()
+    /*
+      enforce the colorChange in case only hex is given it will create every data needed
+    */
+    this.colors = _colorChange(this.colors)
   },
   methods: {
     colorChange (data, oldHue) {
-      if (data.a && data.a > 1) {
-        data.a = 1
-      }
-
-      var color = data.hex ? tinycolor(data.hex) : tinycolor(data)
-      var hsl = color.toHsl()
-      var hsv = color.toHsv()
-      if (hsl.s === 0) {
-        hsl.h = oldHue || 0
-        hsv.h = oldHue || 0
-      }
-      this.colors = {
-        hsl: hsl,
-        hex: color.toHexString().toUpperCase(),
-        rgba: color.toRgb(),
-        hsv: hsv,
-        oldHue: data.h || oldHue || hsl.h,
-        source: data.source,
-        a: data.a
-      }
+      this.colors = _colorChange(data, oldHue)
     },
     isValidHex (hex) {
       return tinycolor(hex).isValid()
