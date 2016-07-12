969df5f75bff7beef30ce1ebb0948f8720b50295
xiaokai <kexiaokai@gmail.com>
2016-07-12 23:24:22 +0800

    chore: change dev webpack

--- node_modules/vue/src/util/dom.js
+++ node_modules/vue/src/util/dom.js
@@ -37,8 +37,9 @@ export function query (el) {
  */
 
 export function inDoc (node) {
-  var doc = document.documentElement
-  var parent = node && node.parentNode
+  if (!node) return false
+  var doc = node.ownerDocument.documentElement
+  var parent = node.parentNode
   return doc === node ||
     doc === parent ||
     !!(parent && parent.nodeType === 1 && (doc.contains(parent)))
