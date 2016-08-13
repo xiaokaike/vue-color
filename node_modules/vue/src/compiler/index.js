977bbad3103afa6beecf4127e2b2fbd139a65f3e
xiaokai <kexiaokai@gmail.com>
2016-08-13 12:50:29 +0800

    style: delete node modules

--- node_modules/vue/src/compiler/index.js
+++ node_modules/vue/src/compiler/index.js
@@ -1,3 +1,22 @@
-export * from './compile'
-export * from './transclude'
-export * from './resolve-slots'
+/* @flow */
+
+import { parse } from './parser/index'
+import { optimize } from './optimizer'
+import { generate } from './codegen/index'
+
+/**
+ * Compile a template.
+ */
+export function compile (
+  template: string,
+  options: CompilerOptions
+): CompiledResult {
+  const ast = parse(template.trim(), options)
+  optimize(ast, options)
+  const code = generate(ast, options)
+  return {
+    ast,
+    render: code.render,
+    staticRenderFns: code.staticRenderFns
+  }
+}
