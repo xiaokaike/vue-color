b19350fc62a9ac0078bf94c524d05385d2d8dc47
xiaokai <kexiaokai@gmail.com>
2017-03-31 20:58:21 +0800

    feat: merge

--- example/App.vue
+++ example/App.vue
@@ -6,7 +6,7 @@
       <header class="header">
         <div class="intro-wrap">
           <div class="intro">
-            <h1>Vue-color</h1>
+            <h1>Vue-colorfsdfds</h1>
             <p>A Collection of Color Pickers from Sketch, Photoshop, Chrome, Github, Twitter, Material Design & more</p>
           </div>  
         </div> 
@@ -55,127 +55,127 @@
 </template>
 
 <script>
-import material from '../src/components/Material.vue'
-import compact from '../src/components/Compact.vue'
-import swatches from '../src/components/Swatches.vue'
-import slider from '../src/components/Slider.vue'
-import sketch from '../src/components/Sketch.vue'
-import chrome from '../src/components/Chrome.vue'
-import photoshop from '../src/components/Photoshop.vue'
+  import material from '../src/components/Material.vue'
+  import compact from '../src/components/Compact.vue'
+  import swatches from '../src/components/Swatches.vue'
+  import slider from '../src/components/Slider.vue'
+  import sketch from '../src/components/Sketch.vue'
+  import chrome from '../src/components/Chrome.vue'
+  import photoshop from '../src/components/Photoshop.vue'
 
-let defaultProps = {
-  hex: '#194d33',
-  hsl: {
-    h: 150,
-    s: 0.5,
-    l: 0.2,
-    a: 1
-  },
-  hsv: {
-    h: 150,
-    s: 0.66,
-    v: 0.30,
-    a: 1
-  },
-  rgba: {
-    r: 25,
-    g: 77,
-    b: 51,
-    a: 1
-  },
-  a: 1
-}
-
-export default {
-  components: {
-    'material-picker': material,
-    'compact-picker': compact,
-    'swatches-picker': swatches,
-    'slider-picker': slider,
-    'sketch-picker': sketch,
-    'chrome-picker': chrome,
-    'photoshop-picker': photoshop
-  },
-  data () {
-    return {
-      colors: defaultProps
-    }
-  },
-  computed: {
-    bgc () {
-      return this.colors.hex
-    }
-  },
-  methods: {
-    onOk () {
-      console.log('ok')
+  let defaultProps = {
+    hex: '#194d33',
+    hsl: {
+      h: 150,
+      s: 0.5,
+      l: 0.2,
+      a: 1
     },
-    onCancel () {
-      console.log('cancel')
+    hsv: {
+      h: 150,
+      s: 0.66,
+      v: 0.30,
+      a: 1
     },
-    onChange (val) {
-      this.colors = val
-    }
-  },
-  created () {
+    rgba: {
+      r: 25,
+      g: 77,
+      b: 51,
+      a: 1
+    },
+    a: 1
   }
-}
+
+  export default {
+    components: {
+      'material-picker': material,
+      'compact-picker': compact,
+      'swatches-picker': swatches,
+      'slider-picker': slider,
+      'sketch-picker': sketch,
+      'chrome-picker': chrome,
+      'photoshop-picker': photoshop
+    },
+    data () {
+      return {
+        colors: defaultProps
+      }
+    },
+    computed: {
+      bgc () {
+        return this.colors.hex
+      }
+    },
+    methods: {
+      onOk () {
+        console.log('ok')
+      },
+      onCancel () {
+        console.log('cancel')
+      },
+      onChange (val) {
+        this.colors = val
+      }
+    },
+    created () {
+    }
+  }
 </script>
 
 <style lang="stylus">
-*
-  margin 0
-  padding 0
-html
-  font-family 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif
-.header-container
-  position relative
-  .header-bg
-    position absolute
-    top 0
-    left 0
-    right 0
-    min-height 580px
-    background-color #333
-    opacity 0.5
-    z-index 0
-  .header
-    display flex
-    max-width 780px
-    margin 0 auto
-    padding 20px 0
-    .intro-wrap
-      flex 1
-      margin-right 100px
-    .intro
-      width 300px
-      color rgba(0, 0, 0, 0.65098)
-      > h1
-        font-size 40px
-        font-weight normal
-        line-height 60px
-      > p
-        font-size 16px
-        font-weight normal
-        line-height 22px
-  
-.demo-container
-  max-width 780px
-  min-height 800px
-  margin 0 auto
-  position relative
-  z-index 2
-  .demo-list
-    display flex
-    margin-bottom 20px
-.demo-item
-  position relative
-  margin-bottom 10px
-  margin 0 10px 0 10px
-  z-index 2
-  h6 
+  *
     margin 0
-    padding 5px 0
-    color #666
+    padding 0
+  html
+    font-family 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif
+  .header-container
+    position relative
+    .header-bg
+      position absolute
+      top 0
+      left 0
+      right 0
+      min-height 580px
+      background-color #333
+      opacity 0.5
+      z-index 0
+    .header
+      display flex
+      max-width 780px
+      margin 0 auto
+      padding 20px 0
+      .intro-wrap
+        flex 1
+        margin-right 100px
+      .intro
+        width 300px
+        color rgba(0, 0, 0, 0.65098)
+        > h1
+          font-size 40px
+          font-weight normal
+          line-height 60px
+        > p
+          font-size 16px
+          font-weight normal
+          line-height 22px
+    
+  .demo-container
+    max-width 780px
+    min-height 800px
+    margin 0 auto
+    position relative
+    z-index 2
+    .demo-list
+      display flex
+      margin-bottom 20px
+  .demo-item
+    position relative
+    margin-bottom 10px
+    margin 0 10px 0 10px
+    z-index 2
+    h6 
+      margin 0
+      padding 5px 0
+      color #666
 
 </style>
