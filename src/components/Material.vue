d0eac7a9c684e0e3ded8b7cb93e88b652c9f5bf0
Liu Xinran <xinran.liu@shopeemobile.com>
2017-07-25 19:08:38 +0800

    refactor(Material.vue): sylus -> css

--- src/components/Material.vue
+++ src/components/Material.vue
@@ -1,19 +1,19 @@
 <template>
 
-  <div class="vue-color__material">
-    <ed-in class="vue-color__material__hex" label="hex" v-model="colors.hex"
+  <div class="c-material">
+    <ed-in class="hex" label="hex" v-model="colors.hex"
       :style="{ borderColor: colors.hex }" @change="onChange"></ed-in>
 
-    <div class="vue-color__material__split">
-      <div class="vue-color__material__third">
+    <div class="split">
+      <div class="third">
         <ed-in label="r" v-model="colors.rgba.r"
         @change="onChange"></ed-in>
       </div>
-      <div class="vue-color__material__third">
+      <div class="third">
         <ed-in label="g" v-model="colors.rgba.g"
         @change="onChange"></ed-in>
       </div>
-      <div class="vue-color__material__third">
+      <div class="third">
         <ed-in label="b" v-model="colors.rgba.b"
         @change="onChange"></ed-in>
       </div>
@@ -56,37 +56,46 @@ export default {
 }
 </script>
 
-<style lang="stylus">
-.vue-color__material
-  width 98px
-  height 98px
-  padding 16px
-  font-family "Roboto"
-  position relative
-  border-radius 2px
-  box-shadow 0 2px 10px rgba(0,0,0,.12), 0 2px 5px rgba(0,0,0,.16)
-  background-color #fff
-  .vue-color__editable-input__input
-    width 100%
-    margin-top 12px
-    font-size 15px
-    color #333
-    height 30px
-  .vue-color__editable-input__label
-    position absolute
-    top 0
-    left 0
-    font-size 11px
-    color #999
-    text-transform capitalize
-.vue-color__material__hex
-  border-bottom-width 2px
-  border-bottom-style solid
-.vue-color__material__split
-  display flex
-  margin-right -10px
-  padding-top 11px
-.vue-color__material__third
-  flex 1
-  padding-right 10px
+<style scoped>
+.c-material {
+  width: 98px;
+  height: 98px;
+  padding: 16px;
+  font-family: "Roboto";
+  position: relative;
+  border-radius: 2px;
+  box-shadow: 0 2px 10px rgba(0,0,0,.12), 0 2px 5px rgba(0,0,0,.16);
+  background-color: #fff;
+}
+
+.c-material >>> .input__input {
+  width: 100%;
+  margin-top: 12px;
+  font-size: 15px;
+  color: #333;
+  height: 30px;
+}
+
+.c-material >>> .input__label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  font-size: 11px;
+  color: #999;
+  text-transform: capitalize;
+}
+
+.hex {
+  border-bottom-width: 2px;
+  border-bottom-style: solid;
+}
+.split {
+  display: flex;
+  margin-right: -10px;
+  padding-top: 11px;
+}
+.third {
+  flex: 1;
+  padding-right: 10px;
+}
 </style>
