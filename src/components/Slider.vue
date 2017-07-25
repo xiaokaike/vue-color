4a64b5f28f00522ed37b442f112d95d900d6a61f
Liu Xinran <xinran.liu@shopeemobile.com>
2017-07-25 18:49:08 +0800

    refactor(Slider.vue): sylus -> css

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="vue-color__slider">
-    <div class="vue-color__slider__hue-warp">
+  <div class="c-slider">
+    <div class="hue-warp">
       <hue v-model="colors" @change="hueChange"></hue>
     </div>
-    <div class="vue-color__slider__swatches">
-      <div class="vue-color__slider__swatch" v-for="(offset, index) in swatches" :data-index="index"
+    <div class="swatches">
+      <div class="swatch" v-for="(offset, index) in swatches" :data-index="index"
         @click="handleSwClick(index, offset)">
-        <div class="vue-color__slider__swatch-picker"
-        :class="{'vue-color__slider__swatch-picker--active': offset == activeOffset}"
+        <div class="swatch-picker"
+        :class="{'swatch-picker--active': offset == activeOffset}"
         :style="{background: 'hsl(' + colors.hsl.h + ', 50%, ' + (offset * 100) + '%)'}"
         ></div>
       </div>
@@ -57,39 +57,50 @@ export default {
 }
 </script>
 
-<style lang="stylus">
-.vue-color__slider
-  position relative
-  width 410px
-.vue-color__slider__hue-warp
-  height 12px
-  position relative
-  .vue-color__c-hue__picker
-    width 14px
-    height 14px
-    border-radius 6px
-    transform translate(-7px, -2px)
-    background-color rgb(248, 248, 248)
-    box-shadow 0 1px 4px 0 rgba(0, 0, 0, 0.37)
-.vue-color__slider__swatches
-  display flex
-  margin-top 20px
-.vue-color__slider__swatch
-  margin-right 1px
-  flex 1
-  width 20%
-  &:first-child
-    margin-right 1px
-    .vue-color__slider__swatch-picker
-      border-radius 2px 0px 0px 2px
-  &:last-child
-    margin-right 0
-    .vue-color__slider__swatch-picker
-      border-radius 0px 2px 2px 0px
-.vue-color__slider__swatch-picker
-  cursor pointer
-  height 12px
-.vue-color__slider__swatch-picker--active
-  transform scaleY(1.8)
-  border-radius: 3.6px/2px
+<style scoped>
+.c-slider {
+  position: relative;
+  width: 410px;
+}
+.hue-warp {
+  height: 12px;
+  position: relative;
+}
+ .hue-warp >>> .picker {
+  width: 14px;
+  height: 14px;
+  border-radius: 6px;
+  transform: translate(-7px, -2px);
+  background-color: rgb(248, 248, 248);
+  box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.37);
+} 
+.swatches {
+  display: flex;
+  margin-top: 20px;
+}
+.slider__swatch {
+  margin-right: 1px;
+  flex: 1;
+  width: 20%;
+}
+.slider__swatch:first-child {
+  margin-right: 1px;
+}
+.slider__swatch:first-child .slider__swatch-picker {
+  border-radius: 2px 0px 0px 2px;
+}
+.slider__swatch:last-child {
+  margin-right: 0;
+}
+.slider__swatch:last-child .slider__swatch-picker {
+  border-radius: 0px 2px 2px 0px;
+}
+.slider__swatch-picker {
+  cursor: pointer;
+  height: 12px;
+}
+.slider__swatch-picker--active {
+  transform: scaleY(1.8);
+  border-radius: 3.6px/2px;
+}
 </style>
