63299f66c0b3bab6556e86de22aaa8ec04b6f2ec
Liu Xinran <xinran.liu@shopeemobile.com>
2017-07-26 18:13:56 +0800

    refactor: debug 'release' script done

--- build/webpack.release.js
+++ build/webpack.release.js
@@ -1,9 +1,9 @@
-var vue = require('vue-loader')
 var path = require('path')
-var webpack = require("webpack")
-var ExtractTextPlugin = require("extract-text-webpack-plugin")
-var projectRoot = path.resolve(__dirname, '../')
-var cssLoader = ExtractTextPlugin.extract('style-loader', 'css-loader')
+var vueLoaderConfig = require('./vue-loader.conf')
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
 
 module.exports = {
   entry: {
@@ -15,66 +15,18 @@ module.exports = {
     libraryTarget: 'umd'
   },
   module: {
-    preLoaders: [
+    rules: [
       {
         test: /\.vue$/,
-        loader: 'eslint',
-        include: projectRoot,
-        exclude: /node_modules/
+        loader: 'vue-loader',
+        options: vueLoaderConfig
       },
       {
         test: /\.js$/,
-        loader: 'eslint',
-        include: projectRoot,
+        loader: 'babel-loader',
+        include: [resolve('src')],
         exclude: /node_modules/
-      }
-    ],
-    loaders: [
-      {
-        test: /\.vue$/,
-        loader: 'vue'
-      },
-      {
-        test: /\.js$/,
-        // excluding some local linked packages.
-        // for normal use cases only node_modules is needed.
-        exclude: /node_modules/,
-        loader: 'babel'
-      },
-      {
-        test: /\.css$/,
-        loader: cssLoader
-      },
-      {
-        test: /\.styl$/,
-        loader: ExtractTextPlugin.extract('style-loader','css-loader!stylus-loader')
       }
     ]
-  },
-  eslint: {
-    formatter: require('eslint-friendly-formatter')
-  },
-  babel: {
-    presets: ['es2015'],
-    plugins: ['transform-runtime']
   }
-}
-
-if (process.env.NODE_ENV === 'production') {
-
-  delete module.exports.devtool
-  module.exports.plugins = [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.optimize.OccurenceOrderPlugin()
-    // new ExtractTextPlugin('build.css')
-  ]
 }
