558ae6510c2849e025d0da26aa620e49506ce291
Liu Xinran <liuxinran1008@gmail.com>
2017-10-30 11:31:35 +0800

    chore: check multiple color formats

--- example/App.vue
+++ example/App.vue
@@ -66,20 +66,17 @@ let defaultProps = {
   hsl: {
     h: 150,
     s: 0.5,
-    l: 0.2,
-    a: 1
+    l: 0.2
   },
   hsv: {
     h: 150,
     s: 0.66,
-    v: 0.30,
-    a: 1
+    v: 0.30
   },
   rgba: {
     r: 25,
     g: 77,
-    b: 51,
-    a: 1
+    b: 51
   },
   a: 1
 }
@@ -112,7 +109,6 @@ export default {
       console.log('cancel')
     },
     updateValue (value) {
-      // TODO: 格式转换
       this.color = value
     }
   },
