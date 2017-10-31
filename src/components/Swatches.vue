f0935a218352dc6d160883a92ca1f707210a3f7f
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-10-30 22:07:04 -0500

    Add props colorLevels, colorMap, and removeColors to make Swatches component more customizable

--- src/components/Swatches.vue
+++ src/components/Swatches.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="vc-swatches" :data-pick="pick">
     <div class="vc-swatches-box">
-      <div class="vc-swatches-color-group" v-for="(group, $idx) in palette" :key="$idx">
+      <div class="vc-swatches-color-group" v-for="(group, $idx) in filteredColors" :key="$idx">
         <div :class="['vc-swatches-color-it', {'vc-swatches-color--white': c === '#FFFFFF' }]"
           v-for="c in group" :key="c"
           :data-color="c"
@@ -22,45 +22,60 @@
 import material from 'material-colors'
 import colorMixin from '../mixin/color'
 
-var colorMap = [
-  'red', 'pink', 'purple', 'deepPurple',
-  'indigo', 'blue', 'lightBlue', 'cyan',
-  'teal', 'green', 'lightGreen', 'lime',
-  'yellow', 'amber', 'orange', 'deepOrange',
-  'brown', 'blueGrey', 'black'
-]
-var colorLevel = ['900', '700', '500', '300', '100']
-var defaultColors = (() => {
-  var colors = []
-  colorMap.forEach((type) => {
-    var typeColor = []
-    if (type.toLowerCase() === 'black' || type.toLowerCase() === 'white') {
-      typeColor = typeColor.concat(['#000000', '#FFFFFF'])
-    } else {
-      colorLevel.forEach((level) => {
-        const color = material[type][level]
-        typeColor.push(color.toUpperCase())
-      })
-    }
-    colors.push(typeColor)
-  })
-  return colors
-})()
-
 export default {
   name: 'Swatches',
   mixins: [colorMixin],
   props: {
-    palette: {
+    colorLevels: {
       type: Array,
       default () {
-        return defaultColors
+        return ['900', '700', '500', '300', '100']
+      }
+    },
+    colorMap: {
+      type: Array,
+      default () {
+        return [
+          'red', 'pink', 'purple', 'deepPurple',
+          'indigo', 'blue', 'lightBlue', 'cyan',
+          'teal', 'green', 'lightGreen', 'lime',
+          'yellow', 'amber', 'orange', 'deepOrange',
+          'brown', 'blueGrey', 'black'
+        ]
+      }
+    },
+    removeColors: {
+      type: Array,
+      default () {
+        return []
       }
     }
   },
   computed: {
     pick () {
       return this.colors.hex
+    },
+    
+    filteredColors () {
+      var colors = []
+      this.colorMap.forEach((type) => {
+        var typeColor = []
+        if (type.toLowerCase() === 'black' || type.toLowerCase() === 'white') {
+          typeColor = typeColor.concat(['#000000', '#FFFFFF'])
+        } else {
+          this.colorLevels.forEach((level) => {
+            const color = material[type][level]
+            typeColor.push(color.toUpperCase())
+          })
+        }
+        colors.push(typeColor)
+      })
+
+      return colors.map(group => {
+        return group.filter(color => {
+          return this.removeColors.includes(color) ? false : true;
+        })
+      })
     }
   },
   methods: {
