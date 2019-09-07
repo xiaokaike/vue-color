4c2a8a13c391d6b3ec5d5b05fd5e7a921f9afd8d
Ryan Ludwig <rludwig@vernier.com>
2019-09-07 16:02:56 -0700

    show the selected color in the color selection circle

--- src/components/common/Saturation.vue
+++ src/components/common/Saturation.vue
@@ -8,7 +8,7 @@
     <div class="vc-saturation--white"></div>
     <div class="vc-saturation--black"></div>
     <div class="vc-saturation-pointer" :style="{top: pointerTop, left: pointerLeft}">
-      <div class="vc-saturation-circle"></div>
+      <div class="vc-saturation-circle" :style="{background: colors.hex}"></div>
     </div>
   </div>
 </template>
