e50bd210db4affdce6a998aa59a00021e7fb7e9d
Ankit Ahuja <ahuja.ankit@gmail.com>
2020-07-05 16:04:34 -0700

    update release build to include vue-color.min.css

--- build/utils.js
+++ build/utils.js
@@ -46,13 +46,7 @@ exports.cssLoaders = function (options) {
 
   // https://vue-loader.vuejs.org/en/configurations/extract-css.html
   return {
-    css: generateLoaders(),
-    postcss: generateLoaders(),
-    less: generateLoaders('less'),
-    sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
-    stylus: generateLoaders('stylus'),
-    styl: generateLoaders('stylus')
+    css: generateLoaders()
   }
 }
 
