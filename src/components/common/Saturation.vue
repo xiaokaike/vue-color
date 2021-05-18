5a7799dd0140a872a8f746bed97acaca61452c83
hurradieweltgehtunter <hi@florianlenz.com>
2021-05-18 11:04:01 +0200

    fix non-existing css property

--- src/components/common/Saturation.vue
+++ src/components/common/Saturation.vue
@@ -115,7 +115,7 @@ export default {
   position: absolute;
 }
 .vc-saturation-circle {
-  cursor: head;
+  cursor: pointer;
   width: 4px;
   height: 4px;
   box-shadow: 0 0 0 1.5px #fff, inset 0 0 1px 1px rgba(0,0,0,.3), 0 0 1px 2px rgba(0,0,0,.4);
